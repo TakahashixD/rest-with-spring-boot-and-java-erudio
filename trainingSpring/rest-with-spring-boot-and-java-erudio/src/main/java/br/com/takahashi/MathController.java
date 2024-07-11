0e461de3e8fd8d9f3c4f4bc298d3605a264fb0b2
Diego <diego.taka@hotmail.com>
2024-07-11 16:31:46 -0300

    finished math service for the controller

--- trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/MathController.java
+++ trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/MathController.java
@@ -6,33 +6,75 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.takahashi.exceptions.UnsupportedMathOperationException;
+import br.com.takahashi.service.MathService;
 
 @RestController
 public class MathController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-	
+	private MathService mathService = new MathService();
 	@GetMapping(value = "/sum/{numberOne}/{numberTwo}")
 	public Double sum(
 			@PathVariable(value="numberOne") String numberOne,
-			@PathVariable(value="numberOne") String numberTwo
+			@PathVariable(value="numberTwo") String numberTwo
 			) throws Exception {
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Please set a correct numeric value");
 		}
-		return converToDouble(numberOne) + converToDouble(numberTwo);
+		return mathService.sum(numberOne, numberTwo);
+	}
+	
+	@GetMapping(value = "/sub/{numberOne}/{numberTwo}")
+	public Double sub(
+			@PathVariable(value="numberOne") String numberOne,
+			@PathVariable(value="numberTwo") String numberTwo
+			) throws Exception {
+		if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a correct numeric value");
+		}
+		return mathService.sub(numberOne, numberTwo);
 	}
 
-	private Double converToDouble(String strNumber) {
-		if(strNumber == null) return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if(isNumeric(number)) return Double.parseDouble(number);
-		return 0D;
+	@GetMapping(value = "/mul/{numberOne}/{numberTwo}")
+	public Double mul(
+			@PathVariable(value="numberOne") String numberOne,
+			@PathVariable(value="numberTwo") String numberTwo
+			) throws Exception {
+		if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a correct numeric value");
+		}
+		return mathService.mul(numberOne, numberTwo);
 	}
-
-	private boolean isNumeric(String strNumber) {
-		if(strNumber == null) return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	
+	@GetMapping(value = "/div/{numberOne}/{numberTwo}")
+	public Double div(
+			@PathVariable(value="numberOne") String numberOne,
+			@PathVariable(value="numberTwo") String numberTwo
+			) throws Exception {
+		if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a correct numeric value");
+		}
+		return mathService.div(numberOne, numberTwo);
+	}
+	
+	@GetMapping(value = "/media/{numberOne}/{numberTwo}")
+	public Double media(
+			@PathVariable(value="numberOne") String numberOne,
+			@PathVariable(value="numberTwo") String numberTwo
+			) throws Exception {
+		if(!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a correct numeric value");
+		}
+		return mathService.media(numberOne, numberTwo);
+	}
+	
+	@GetMapping(value = "/raiz/{number}")
+	public Double raiz(
+			@PathVariable(value="number") String number
+			) throws Exception {
+		if(!mathService.isNumeric(number)) {
+			throw new UnsupportedMathOperationException("Please set a correct numeric value");
+		}
+		return mathService.raiz(number);
 	}
 }
