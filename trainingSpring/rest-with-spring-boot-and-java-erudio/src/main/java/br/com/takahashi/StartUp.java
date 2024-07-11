504be7907d82e9f65e59035ecb171f77851667d8
Diego <diego.taka@hotmail.com>
2024-07-11 14:50:34 -0300

    simple endpoint mock

---
+++ trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.takahashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
