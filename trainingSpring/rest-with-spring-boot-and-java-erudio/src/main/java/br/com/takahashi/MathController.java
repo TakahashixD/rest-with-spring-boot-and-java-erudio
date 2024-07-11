c3bbcc1ec87c62710289b5c1dc10b22966607876
Diego <diego.taka@hotmail.com>
2024-07-11 15:49:53 -0300

    custo error exception

--- trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/MathController.java
+++ trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/MathController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.takahashi.exceptions.UnsupportedMathOperationException;
 
 @RestController
 public class MathController {
@@ -17,18 +18,21 @@ public class MathController {
 			@PathVariable(value="numberOne") String numberTwo
 			) throws Exception {
 		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new UnsupportedMathOperationException("Please set a correct numeric value");
 		}
 		return converToDouble(numberOne) + converToDouble(numberTwo);
 	}
 
-	private int converToDouble(String strNumber) {
-		// TODO Auto-generated method stub
-		return 0;
+	private Double converToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if(isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
 	}
 
 	private boolean isNumeric(String strNumber) {
-		// TODO Auto-generated method stub
-		return false;
+		if(strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
