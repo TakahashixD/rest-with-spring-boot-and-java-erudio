50168b761e0bb934f501715488e2b4e2dceb53df
Diego <diego.taka@hotmail.com>
2024-07-12 16:47:07 -0300

    simple crud without db

--- trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/controllers/PersonController.java
+++ trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/controllers/PersonController.java
@@ -1,14 +1,21 @@
 package br.com.takahashi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.takahashi.model.Person;
 import br.com.takahashi.services.PersonService;
+import ch.qos.logback.classic.Logger;
 
 @RestController
 @RequestMapping("/person")
@@ -17,8 +24,33 @@ public class PersonController {
 	@Autowired
 	private PersonService personService;
 	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return personService.findAll();
+	}
+	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value="id") String id) {
 		return personService.findById(id);
 	}
+	
+	@PostMapping( 
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {		
+		return personService.create(person);
+	}
+	
+	@PutMapping( 
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {		
+		return personService.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable(value="id") String id) {
+		personService.delete(id);
+	}
+	
 }
--- trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/services/PersonService.java
+++ trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/services/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.takahashi.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,15 @@ public class PersonService {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
 	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		for(int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+	
 	public Person findById(String id) {
 		logger.info("Finding one person");
 		Person person = new Person();
@@ -20,6 +31,30 @@ public class PersonService {
 		person.setLastName("Takahashi");
 		person.setGender("Male");
 		person.setAddress("Maringá, PR");
+		return person;
+	}
+	
+	public Person create(Person person) {
+		logger.info("create one person");
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("update one person");
+		return person;
+	}
+	
+	public void delete(String id) {
+		logger.info("delete one person");
+	}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person Name" + i);
+		person.setLastName("Last Name" + i);
+		person.setGender("Male");
+		person.setAddress("Some address in Brazil" + i);
 		return person;
 	}
 }
