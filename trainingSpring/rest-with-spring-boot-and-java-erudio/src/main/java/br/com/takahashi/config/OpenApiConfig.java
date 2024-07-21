7441134477f0d59149cbda4027b5229a29e615c5
Diego <diego.taka@hotmail.com>
2024-07-21 11:31:36 -0300

    import openAPI dep and create custom config

---
+++ trainingSpring/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/takahashi/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package br.com.takahashi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+				.title("RESTful API with java 21 and SpringBoot 3")
+				.version("v1")
+				.description("Simple API")
+				.termsOfService("")
+				.license(new License()
+						.name("Apache 2.0")
+						.url("")));
+	}
+}
